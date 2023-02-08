cd33abd49495d40761fc2fc7a1747fcd5e75b134
Solar <115794865+Solaree@users.noreply.github.com>
2023-02-08 11:26:43 +0200

    Delete SolarKit directory

--- SolarKit/dos.js
+++
@@ -1,7 +0,0 @@
-var config = require('./dos_conf.json');
-
-var Stress = require('ddos-stress');
-
-var stress = new Stress();
-
-stress.run(config['server'], config['requests'], config['port'])
--- SolarKit/run_this.bash
+++
@@ -1,7 +0,0 @@
-pip install -r reqirements.txt
-
-sudo npm install -g npm
-
-sudo npm i ddos-stress
-
-python __main__.py
